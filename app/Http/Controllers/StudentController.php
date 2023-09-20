b901a0a27779a641fd23c42ca650c2bd76379a99
dodo-selarassolusindo <119738003+dodo-selarassolusindo@users.noreply.github.com>
2023-09-20 16:37:20 +0700

    .

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        return view("welcome2");
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'addMoreInputFields.*.subject' => 'required'
+        ]);
+
+        foreach ($request->addMoreInputFields as $key => $value) {
+            Student::create($value);
+        }
+
+        return back()->with('success', 'New subject has been added.');
+    }
+}
